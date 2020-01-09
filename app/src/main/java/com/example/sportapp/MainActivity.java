273c803a697c9ae98dfe2bd40710482b92b62136
Paweł Woźniak <WPawel78@gmail.com>
2020-01-09 13:06:33 +0100

    add first on click, deleted unnecessery photos

--- app/src/main/java/com/example/sportapp/MainActivity.java
+++ app/src/main/java/com/example/sportapp/MainActivity.java
@@ -3,15 +3,24 @@ package com.example.sportapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = findViewById(R.id.buttonEasy);
 
+        public void onClick(View v){
+            Toast.makeText(MainActivity.this, "Button clicked", Toast.LENGTH_SHORT).show();
+        }
     }
 }
