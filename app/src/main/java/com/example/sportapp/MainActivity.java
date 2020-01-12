00b2e7e13021b1fd9a214acc89f359d9b7f159d2
Paweł Woźniak <WPawel78@gmail.com>
2020-01-12 17:26:26 +0100

    add activity stoper and onclick function

--- app/src/main/java/com/example/sportapp/MainActivity.java
+++ app/src/main/java/com/example/sportapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sportapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,17 +11,35 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    Button buttonPRo, buttonEasy, buttonHard;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.buttonEasy);
+        buttonEasy = findViewById(R.id.buttonEasy);
+        buttonHard = findViewById(R.id.buttonHard);
+        buttonPRo = findViewById(R.id.buttonPro);
 
-        public void onClick(View v){
-            Toast.makeText(MainActivity.this, "Button clicked", Toast.LENGTH_SHORT).show();
-        }
+
+        buttonEasy.setOnClickListener(new View.OnClickListener() { //dodanie funkcji button przejścia Stopera
+            @Override
+            public void onClick(View v) {
+                moveToStoper();
+            }
+        });
+        
+
+    }
+
+    private void moveToStoper(){
+        Intent intent = new Intent(MainActivity.this, Stoper.class);
+        startActivity(intent);
+    }
+
+    public Button getButtonEasy() {
+        return buttonEasy;
     }
 }
